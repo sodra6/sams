8ba5733ed73af5c193ae958a7d2f9ad89ed14392
Cho Yong Gyu <66618500+sodra6@users.noreply.github.com>
2023-03-13 09:38:17 +0900

    Add files via upload

---
+++ SAMS/front/src/lib/AUIGrid/AUIGrid-Core/AUIGridLicense.js
@@ -0,0 +1,10 @@
+/*!
+ * AUIGrid 3.0.12 License
+ * License Type : Evaluation(Trial) Version
+ * Authorized Domain(or IP) : localhost,127.0.0.1
+ * Expiration Date : 2023-03-16
+ * www.auisoft.net
+ */
+ /* eslint-disable */
+var AUIGridLicense = "eyJjdCI6Ikt5RVgweUczdFdPTW44S3ZKbjBsRXRmMG5qZWp2bGxXbjh1cTVRMnd6cDRKQ2xSdk1HVXVhSjJONDJzeU5oK3M3OFk1N2NxN0hXYVE2RHQyaERTOThieTAxclFDajBVT215TkQ0YUFaZFU0PSIsIml2IjoiZDM1N2ZhMTY2YTZhODdhYjJjNjQ0Y2E5NWY1ZDU3ZTYiLCJzIjoiYzkwY2FlMjczYjIxZTIwMCJ9";
+if (typeof window !== "undefined") window.AUIGridLicense = AUIGridLicense;
